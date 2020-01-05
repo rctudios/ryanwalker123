5267496c75a351adda5bb01a23a9ff44baaef147
rctudios <54410807+rctudios@users.noreply.github.com>
2020-01-05 12:53:16 -0700

    I

---
+++ Python/001/main.py
@@ -0,0 +1,26 @@
+import pip, os
+
+print("Checking package installations, please hold...")
+os.system('pip install pytest -q --user --no-warn-script-location')
+os.system('pip install pytest-sugar -q --user')
+
+import pytest
+import num_gen 
+
+def main():
+    a = ''
+    while a not in ('run','test'):
+        print("Choose an option:\n\tRun\n\tTest")
+        a = input("Your choice: ")
+        a = a.lower()
+    if a=="run":
+        print("\n--Running Program--\n")
+        student.main()
+    elif a=="test":
+        print("\n--Testing Program--\n")
+        pytest.main(['-v'])
+
+if __name__ == '__main__':
+    main()
+    print("\n--Operation Complete--")
+    input("Press the Enter key to exit...")
